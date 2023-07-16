2b454eee97cab4252e146fe5b4c9c27929db7679
Ibrahim Doghri <patron.bara@gmail.com>
2023-07-16 11:23:00 +0100

    cleaning project struct

--- src/routes/projectss/+page.svelte
+++
@@ -1,9 +0,0 @@
-<div>projects</div>
-
-<style>
-    div{
-        color: #fff;
-        border: 4px solid #fff;
-        padding: 1em;
-    }
-</style>
--- src/routes/works/+page.svelte
+++
@@ -1,9 +0,0 @@
-<div>work</div>
-
-<style>
-    div{
-        color: #fff;
-        border: 4px solid #fff;
-        padding: 1em;
-    }
-</style>
