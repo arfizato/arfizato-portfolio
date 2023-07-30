2009ec6dc786d9ae33b96b7343d9f338092b3887
Ibrahim Doghri <patron.bara@gmail.com>
2023-07-30 19:42:09 +0100

    work

--- src/lib/deck/work/job.svelte
+++ src/lib/deck/work/job.svelte
@@ -1,11 +1,55 @@
 <script lang="ts">
+    import { page } from "$app/stores";
     export let data: any;
+    const imagepath = `${$page.url.origin}/api?file=work/${data.imgpath}`;
 </script>
-
-
 <section id={data.id}>
-    <h1>{data.title}</h1>
-    <h6>{data.employer}</h6>
-    <p>{data.description}</p>
-
+    <div class="jobContainer">
+        <div class="textContainer">
+            <h1>{data.title}</h1>
+            <h6 class="offbitDotFont employer">{data.employer}</h6>
+            <p>{data.description}</p>
+        </div> 
+        {#if data.imgpath!== ""}
+            <img src={imagepath} alt={data.title} class="jobImg">
+        {/if}
+    </div>
 </section>
+
+<style>
+.jobContainer {
+    width: calc(100% - 3vmin - 42px);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 20px;
+    /* margin: 0 0 5rem 0; */
+}
+.textContainer {
+    /* width: 50%; */
+}
+
+.employer{
+    font-size: 1em;
+    letter-spacing: 2px;
+    font-weight: 100;
+}
+.jobImg{
+    width: 50%;
+    max-width: 450px;
+    height: auto;
+    object-fit: contain;
+    /* filter: drop-shadow(1px 1px  0 #fff) drop-shadow(-1px -1px  0 #fff) drop-shadow(-1px 1px  0 #fff) drop-shadow(1px -1px  0 #fff); */
+    /* border: var(--r-main-color) 2px solid; */
+}
+section{
+    width: 100%;
+    top: 0 !important;
+    /* align-self: center; */
+    height: 100%;
+    display: flex !important;
+    justify-content: center;
+    align-items: stretch;
+}
+</style>
--- src/lib/deck/work/work.svelte
+++ src/lib/deck/work/work.svelte
@@ -1,16 +1,25 @@
 <script>
     import Job from "./job.svelte";
+    import {items} from "../../assets/data/work.json";
 </script>
 
+
 <section id="work">
-    <!-- Hello Projects! -->
-    <Job data={{
-        id:"one", title:"Summer Internship", employer:"Sopra Steria", description:"I worked as a full stack developer in a team of 4 people. We developed a web application for a client using React, Spring Boot and PostgreSQL."
-    }} />
-    <Job data={{
-        id:"two", title:"Full Stack Developer", employer:"ba9chich", description:"I worked as a full stack developer in a team of 4 people. We developed a web application for a client using React, Spring Boot and PostgreSQL."
-    }} />
-    <Job data={{
-        id:"three", title: "Data Scientist", employer:"self",description: "li yestanna khir melli yetmanna"
-    }} />
+    <!-- <Job data={{
+        id:"job-"+99, 
+        title:"Summer Internship", 
+        employer:"Sopra Steria", 
+        imgpath: "",
+        description:"I worked as a full stack developer in a team of 4 people. We developed a web application for a client using React, Spring Boot and PostgreSQL."
+    }} /> -->
+    {#each items as item}
+        <Job data={{id:"job-"+item.title.replace(" ","-"), ...item  }} />
+    {/each}
 </section>
+
+<style>
+    #work{
+        width: 100%;
+        height: 100%;
+    }
+</style>
