f902997165c36197bbf648c658af9761885f2f63
Ibrahim Doghri <patron.bara@gmail.com>
2023-07-30 19:41:19 +0100

    about

--- src/lib/deck/about/about.svelte
+++ src/lib/deck/about/about.svelte
@@ -1,15 +1,38 @@
 <script>
-
+    import MyImage from "../../assets/images/fullmoon.jpg";
+    import Socials from "./socials.svelte";
+    import about from "../../assets/data/about.json";
 </script>
 
-<svelte:head>
-    <title>About</title>
-</svelte:head>
 
 <section id="about">
-    <h1>About me.</h1>
-    <h3>Ibrahim (Arfizato) Doghri</h3>
-    <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque nisi, cumque exercitationem iure deleniti accusamus. Laborum nobis ex reiciendis doloribus ad itaque, aliquid enim voluptatem cum a corporis inventore iure.
-    </p>
+    <div class="aboutSubContainer">
+        <div class="contentContainer">
+            <h2>{about.name}</h2>
+            <Socials data={about.socials}/>
+            <p>{about.bio}</p>
+        </div>
+        <img src={MyImage} alt="of ibrahim serving the audio gods">
+    </div>
 </section>
+
+<style>
+    h1{
+        font-size: 1rem;
+        /* font-weight: 700;
+        margin-bottom: 1rem; */
+    }
+    .aboutSubContainer{
+        display: grid;
+        align-items: center;
+        justify-items: center;
+        
+        grid-template-columns: 2.5fr auto;
+        gap: 20px;
+        /* height: 70vh; */
+    }
+    img{
+        height: 70vh; 
+        border: var(--r-main-color) 2px solid;
+    }
+</style>
---
+++ src/lib/deck/about/socials.svelte
@@ -0,0 +1,36 @@
+<script>
+// @ts-nocheck
+    import { page } from "$app/stores";
+    export let data;
+
+    
+</script>
+
+<div class="socialsContainer">
+    {#each data as social}
+        <a href={social.url} target="_blank" rel="noreferrer" >
+            <img src={`${$page.url.origin}/api?file=${social.img}`}  alt={social.alt}>
+        </a>
+    {/each}
+</div>
+
+<style>
+    .socialsContainer{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 30px;
+        width: 100%;
+        height: 100%;
+    }
+    img{
+        width: 50px;
+        height: 50px;
+        transition: all 0.3s ease-in-out;
+        filter: invert(1);
+        margin: 0 !important;
+    }
+    img:hover{
+        transform: scale(0.9);
+    }
+</style>
