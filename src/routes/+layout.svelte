82a9b97d74f08f061380e6ea121cb6a15ec411fb
Ibrahim Doghri <patron.bara@gmail.com>
2023-08-13 16:38:27 +0100

    undo navlinks direction: rosy said it's cooler the other way

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -157,7 +157,7 @@
             /* transform: rotateZ(90deg) translateX(10%); */
             writing-mode: vertical-rl;
             justify-content: center;
-            flex-direction: column-reverse;
+            /* flex-direction: column-reverse; */
         }
         .navLinks a:hover, .navLinks .current{
             transform: translateY(.5em);
