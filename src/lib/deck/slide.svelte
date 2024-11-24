a06ecd0961eac0d26fe7f96291736a9154221b1c
Arfizato <33662900+arfizato@users.noreply.github.com>
2024-11-24 23:49:04 +0100

    feat: update info + formatting

--- src/lib/deck/slide.svelte
+++ src/lib/deck/slide.svelte
@@ -1,128 +1,115 @@
 <script>
 	// @ts-nocheck
-/* ------------------------------- Components ------------------------------- */
+	/* ------------------------------- Components ------------------------------- */
 	import { onMount } from 'svelte';
-    import About from './about/about.svelte';
-    import Main from './home/home.svelte';
-    import Projects from './projects/projects.svelte';
-    import Work from './work/work.svelte';
+	import About from './about/about.svelte';
+	import Main from './home/home.svelte';
+	import Projects from './projects/projects.svelte';
+	import Work from './work/work.svelte';
 	import Contact from './contact/contact.svelte';
-    import { pageIndex } from '@routes/stores.ts';
-	import { page } from "$app/stores";
+	import { pageIndex } from '@routes/stores.ts';
+	import { page } from '$app/stores';
 
-
-    /* --------------------------------- plugins -------------------------------- */
-	import Reveal from 'reveal.js'; 
+	/* --------------------------------- plugins -------------------------------- */
+	import Reveal from 'reveal.js';
 	import Animate from '../plugins/animate/animate.esm.js';
 	import Verticator from '../plugins/verticator/plugin/verticator/verticator.esm.js';
-	// import Markdown from 'reveal.js/plugin/markdown/markdown';
-	// import Highlight from 'reveal.js/plugin/highlight/highlight';
-	// import Notes from 'reveal.js/plugin/notes/notes';
-	// import Simplemenu from 'reveal.js/plugin/simplemenu/plugin/simplemenu/simplemenu';
-	// import 'reveal.js/plugin/simplemenu/plugin/simplemenu/simplemenu.css';
 
-/* ------------------------------- stylesheets ------------------------------ */
-    import 'animate.css';
-    import 'reveal.js/dist/reveal.css';
-    import './arfizato.css';
-    // import 'reveal.js/dist/theme/black.css';
-    import 'reveal.js/plugin/highlight/monokai.css';
-    import verticatorCssPath from '../plugins/verticator/plugin/verticator/verticator.css?url';
+	/* ------------------------------- stylesheets ------------------------------ */
+	import 'animate.css';
+	import 'reveal.js/dist/reveal.css';
+	import './arfizato.css';
+	import 'reveal.js/plugin/highlight/monokai.css';
+	import verticatorCssPath from '../plugins/verticator/plugin/verticator/verticator.css?url';
 	import { init } from 'svelte/internal';
 
-/* -------------------------------- functions ------------------------------- */
-function initAnimations(deck){
-	
-	// console.log('slidechanged',deck.getCurrentSlide().querySelectorAll('[data-animated], h2'))
-	deck.getCurrentSlide().querySelectorAll('[data-animated]').forEach(item => {
-		// console.log(item)
-		if (item.nodeType==1){            
-			for (let i = item.classList.length - 1; i >= 0; i--) {
-				const className = item.classList[i];
-				if (className.startsWith('animate__')) {
-					item.classList.remove(className);
+	/* -------------------------------- functions ------------------------------- */
+	function initAnimations(deck) {
+		deck
+			.getCurrentSlide()
+			.querySelectorAll('[data-animated]')
+			.forEach((item) => {
+				if (item.nodeType == 1) {
+					for (let i = item.classList.length - 1; i >= 0; i--) {
+						const className = item.classList[i];
+						if (className.startsWith('animate__')) {
+							item.classList.remove(className);
+						}
+					}
+					item.style.display = 'none';
+					item.offsetHeight;
+					item.style.display = 'block';
+					let animation = 'animate__' + item.getAttribute('data-animated');
+					if (item.dataset.repeat != undefined) {
+						item.classList.add('animate__'.concat(item.dataset.repeat));
+					}
+					if (item.dataset.speed != undefined) {
+						item.classList.add('animate__'.concat(item.dataset.speed));
+					}
+					item.classList.add('animate__animated', animation);
 				}
-			}
-			item.style.display='none';
-			item.offsetHeight;
-			item.style.display='block';
-			let animation = 'animate__' + item.getAttribute('data-animated');
-			if(item.dataset.repeat!=undefined){
-				item.classList.add('animate__'.concat(item.dataset.repeat));
-			}
-			if(item.dataset.speed!=undefined){
-				item.classList.add('animate__'.concat(item.dataset.speed));
-			}
-			item.classList.add('animate__animated', animation);
-		}
-		
-	});
+			});
+	}
 
-}
-
-/* ---------------------------- Liyfecycle Events --------------------------- */
+	/* ---------------------------- Liyfecycle Events --------------------------- */
 
 	onMount(() => {
-		// @ts-ignore
 		const deck = new Reveal({
 			autoAnimateEasing: 'ease',
 			autoAnimateDuration: 1,
 			hash: true,
 			controls: true,
 			progress: false,
-            autoSlide: 5000,
-
+			autoSlide: 5000
 		});
 
 		deck.initialize({
-			plugins: [   Animate, Verticator],// Simplemenu],// Notes, Highlight, Markdown,
+			plugins: [Animate, Verticator], // Simplemenu],// Notes, Highlight, Markdown,
 			// animated: {
-				// H1: { animated: 'slideInDown' },
-				// H2: { animated: 'slideInDown', repeat: 'repeat-3' },
-				// IMG: { animated: 'backInDown', repeat: 'repeat-2', speed: 'slower' },
-				// P: { animated: 'bounceInRight', speed: 'fast' },
-				// IMG: { animated: 'jello', repeat: 'infinite', speed: 'slower' }
+			// H1: { animated: 'slideInDown' },
+			// H2: { animated: 'slideInDown', repeat: 'repeat-3' },
+			// IMG: { animated: 'backInDown', repeat: 'repeat-2', speed: 'slower' },
+			// P: { animated: 'bounceInRight', speed: 'fast' },
+			// IMG: { animated: 'jello', repeat: 'infinite', speed: 'slower' }
 			// },
 			verticator: {
 				csspath: verticatorCssPath,
-                clickable: false,
-			},
+				clickable: false
+			}
 			// simplemenu: {
 			// 	csspath: false
 			// }
 		});
 		// console.log("deck",document.querySelectorAll(".slides > section"), $page)
-		if(["","#/home"].includes($page.url.hash))
-			pageIndex.set(1);
+		if (['', '#/home'].includes($page.url.hash)) pageIndex.set(1);
 
-        deck.on( 'slidechanged', event => {
-            // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-            // console.log( "event.indexh:",event.indexh, "event.indexv:",event.indexv); /// "event.previousSlide:",event.previousSlide, "event.currentSlide:",event.currentSlide,
-            pageIndex.set(event.indexh + 1);
+		deck.on('slidechanged', (event) => {
+			// event.previousSlide, event.currentSlide, event.indexh, event.indexv
+			// console.log( "event.indexh:",event.indexh, "event.indexv:",event.indexv); /// "event.previousSlide:",event.previousSlide, "event.currentSlide:",event.currentSlide,
+			pageIndex.set(event.indexh + 1);
 			initAnimations(deck);
-        });
+		});
 	});
 </script>
 
 <div class="reveal">
 	<div class="slides">
-        <Main />
-        <About />
-        <Work />
-        <Projects />
+		<Main />
+		<About />
+		<Work />
+		<Projects />
 		<!-- <Contact /> -->
 	</div>
 </div>
 
-
 <style>
-    .reveal {
-        border: 4px solid var(--r-main-color);
-        width: 100%;
-        height: calc(100dvh - 8px);
+	.reveal {
+		border: 4px solid var(--r-main-color);
+		width: 100%;
+		height: calc(100dvh - 8px);
 	}
 	@media only screen and (max-width: 500px) {
-		.reveal{
+		.reveal {
 			width: calc(100vw - 8px);
 			height: calc(100dvh - 20dvh - 8px);
 		}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,101 +1,47 @@
 <!-- App.svelte -->
-<script lang="ts">// @ts-ignore
-    import intro from "@lottie/intro.json";// @ts-ignore
-    import arrow from "@lottie/arrow.json";
+<script lang="ts">
+	// @ts-ignore
+	import intro from '@lottie/intro.json'; // @ts-ignore
+	import arrow from '@lottie/arrow.json';
 
-    import { browser } from '$app/environment';
-    import { onMount } from "svelte";
-    import lottie from "lottie-web";
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import lottie from 'lottie-web';
 
-    import "$lib/deck/slide.svelte";
-	import Slide from "$lib/deck/slide.svelte";
+	import '$lib/deck/slide.svelte';
+	import Slide from '$lib/deck/slide.svelte';
 
+	let introAnimContainer: HTMLElement, arrowAnimContainer: HTMLElement, arroAnim: any;
 
-    let introAnimContainer: HTMLElement,
-        arrowAnimContainer: HTMLElement,
-        arroAnim: any;
+	onMount(() => {
+		lottie.loadAnimation({
+			container: introAnimContainer,
+			animationData: intro,
+			loop: false
+		});
 
-
-    onMount(()=>{
-        lottie.loadAnimation({
-            container: introAnimContainer,
-            animationData: intro,
-            loop: false,
-        })
-
-        setTimeout(()=>{
-            arroAnim = lottie.loadAnimation({
-                container: arrowAnimContainer,
-                animationData: arrow,
-                loop: false,
-                autoplay: false,
-    
-            })
-        },1700)
-    })
-    function arroAnimation(anim: any, direction: number){
-        // console.log("ha",direction)
-        let lastFrame = anim.totalframes -1;
-        anim.setDirection(direction)
-        anim.goToAndPlay(direction>0? 0 : lastFrame ,true)
-    }
-
+		setTimeout(() => {
+			arroAnim = lottie.loadAnimation({
+				container: arrowAnimContainer,
+				animationData: arrow,
+				loop: false,
+				autoplay: false
+			});
+		}, 1700);
+	});
+	function arroAnimation(anim: any, direction: number) {
+		// console.log("ha",direction)
+		let lastFrame = anim.totalframes - 1;
+		anim.setDirection(direction);
+		anim.goToAndPlay(direction > 0 ? 0 : lastFrame, true);
+	}
 </script>
 
 <svelte:head>
-    <title>Ibrahim DOGHRI's Portfolio</title>
+	<title>Ibrahim DOGHRI's Portfolio</title>
 </svelte:head>
 
-<!-- <div class="animContainer">
-    <div class="introContainer animC" bind:this={introAnimContainer} />
-    <div class="arrowContainer animC" bind:this={arrowAnimContainer} 
-        on:mouseenter={()=>arroAnimation(arroAnim,1)} 
-        on:mouseleave={()=>arroAnimation(arroAnim,-1)} 
-    />
-</div> -->
-<!-- <main>
-    <img class="fog1" src="/fog1.png" alt="fog asset by upklyak on Freepik" >
-    <img class="fog2" src="/fog2.png" alt="fog asset by upklyak on Freepik" >
-    <img class="fog3" src="/fog3.png" alt="fog asset by upklyak on Freepik" >
-    <a href="https://github.com/arfizato">
-      <h1 id="sayMyName">
-        IBRAHIM <br>
-        DOGHRI
-      </h1>
-    </a>
-    <p class="NB">This website is still being designed. Want to take a look at my GitHub Instead?</p>
-</main> -->
-
-<Slide>
-
-</Slide>
-
+<Slide />
 
 <style>
-    /* .animContainer{
-        position: relative; 
-        display: flex;
-        justify-content: center;
-    }
-    .animC{
-        border: 4px solid #fff;
-        height: calc(100vh - 8px);
-        width: calc(100vw - 20vw - 10px);
-        position: absolute;
-    }
-    .arrowContainer{
-        animation: showBoy 2.5s ;
-    }
-    @keyframes showBoy{
-        0%{opacity: 0;}
-        90%{opacity: 0;}
-        100%{opacity: 100;}
-    }
-    @media only screen and (max-width: 500px) {
-        .animC{
-            
-            height: calc(100vh - 20vh- 10px);
-            width: calc(100vw - 8px);
-        }
-    } */
 </style>
